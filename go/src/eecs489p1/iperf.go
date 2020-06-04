82a3cb1421a1dbc9963207c5e73bb0dd71aa2770
Joseph Buiteweg <joebb@umich.edu>
2020-06-03 20:41:49 -0400

    489 p1 in go maybe

---
+++ go/src/eecs489p1/iperf.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/pkg/errors"
+	"github.com/urfave/cli/v2"
+
+	"encoding/gob"
+	"flag"
+)
+
+func main() {
+	fmt.Println("vim-go")
+}
