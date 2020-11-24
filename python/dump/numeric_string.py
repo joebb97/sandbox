8edf49ffc0b07cbb72a1c29551157a705ca662cb
Joseph Buiteweg <joebb@umich.edu>
2020-11-24 18:40:05 -0500

    It's DP problem lol

--- python/dump/numeric_string.py
+++ python/dump/numeric_string.py
@@ -16,7 +16,7 @@
 mapping = {str(i - 96): chr(i) for i in range(97, 123)}
 
 
-def numeric_string(input_str):
+def numeric_string_bad(input_str):
     if len(input_str) == 0:
         return 0
     elif len(input_str) == 1:
@@ -33,8 +33,8 @@ def numeric_string(input_str):
         # "[127]", "[1][27]", "[12][7]", "[1][2][7]" -> 2
         # "[999]", "[9][99]", "[99][9]", "[9][9][9]" -> 1
         # print(input_str[0:len(input_str) - 1], " ", input_str[1:])
-        first_slice = numeric_string(input_str[0:len(input_str) - 1])
-        last_slice = numeric_string(input_str[1:])
+        first_slice = numeric_string_bad(input_str[0:len(input_str) - 1])
+        last_slice = numeric_string_bad(input_str[1:])
         ret = first_slice + last_slice - 1
         if (first_slice > 2 or last_slice > 2) and first_slice != last_slice:
             ret -= 1
@@ -42,13 +42,38 @@ def numeric_string(input_str):
         return ret
 
 
+def numeric_string_dp(input_str):
+    if not input_str:
+        return 0
+
+    ret = [0] * len(input_str)
+    ret[0] = 1
+
+    if len(input_str) >= 2:
+        ret[1] = 2 if input_str[0:2] in mapping else 1
+
+    for i in range(2, len(input_str)):
+        if input_str[i-1: i+1] in mapping:
+            ret[i] = ret[i - 1] + ret[i - 2]
+        else:
+            ret[i] = ret[i - 1]
+
+    return ret[-1]
+
+
+def run_test(func):
+    print("Testing: {}".format(func))
+    assert(func("123") == 3)
+    assert(func("127") == 2)
+    assert(func("999") == 1)
+    assert(func("4123") == 3)
+    assert(func("1234") == 3)
+    assert(func("1278") == 2)
+    assert(func("9999") == 1)
+    assert(func("1212") == 5)
+    assert(func("55555") == 1)
+
+
 if __name__ == '__main__':
-    assert(numeric_string("123") == 3)
-    assert(numeric_string("127") == 2)
-    assert(numeric_string("999") == 1)
-    assert(numeric_string("4123") == 3)
-    assert(numeric_string("1234") == 3)
-    assert(numeric_string("1278") == 2)
-    assert(numeric_string("9999") == 1)
-    assert(numeric_string("1212") == 5)
-    assert(numeric_string("55555") == 1)
+    run_test(numeric_string_dp)
+    run_test(numeric_string_bad)
