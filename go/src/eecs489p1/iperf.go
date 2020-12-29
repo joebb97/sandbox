48ec1e0b4398b58fa12589e0dc65ba465d316695
Joseph Buiteweg <joebb@umich.edu>
2020-12-29 10:39:23 -0500

    Working on iperf

--- go/src/eecs489p1/iperf.go
+++ go/src/eecs489p1/iperf.go
@@ -18,9 +18,9 @@ type Flags struct {
 	useUDP, useTCP       bool
 }
 
-// Open a connection to address addr
+// Open a connection to address addr, which is of format "<IP address>:port"
 func Open(addr string) (*bufio.ReadWriter, error) {
-	log.Println("Dial" + addr)
+	log.Println("Dial " + addr)
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
 		return nil, errors.Wrap(err, "Dialing "+addr+" failed")
@@ -34,16 +34,30 @@ func CheckFlags(flags *Flags) error {
 	if !(flags.isServer || flags.isClient) || (flags.isServer && flags.isClient) {
 		return errors.New("Exactly one of -s or -c must be specified")
 	}
-	// theip := net.ParseIP("192.0.0.-1")
-	// theip == nil
+	return nil
+}
+
+func runServer(flags * Flags) error {
+	return nil
+}
+
+func runClient(flags * Flags) error {
+	wholeAddr := flags.addr + ":" + flags.port
+	rw, err := Open(wholeAddr)
+	if err != nil {
+		return errors.Wrap(err, "Client: Failed to open connection to "+ wholeAddr)
+	}
+	n, err := rw.WriteString("GOOD DAY SIR\n")
+	fmt.Println(n)
 	return nil
 }
 
 // Following tutorial from here: https://appliedgo.net/networking/
 func main() {
 	flags := Flags{useTCP: true}
-	flag.StringVar(&flags.port, "p", ":0", "Port to listen on or connect to")
-	flag.StringVar(&flags.port, "t", "10", "Duration to run client for")
+	flag.StringVar(&flags.port, "p", "5001", "Port to listen on or connect to")
+	flag.StringVar(&flags.addr, "a", "localhost", "Address or hostname of server to connect to.")
+	flag.StringVar(&flags.duration, "t", "10", "Duration to run client for")
 	flag.BoolVar(&flags.isServer, "s", false, "Whether this iperf is a server")
 	flag.BoolVar(&flags.isClient, "c", false, "Whether this iperf is a client")
 	log.Printf("%v", os.Args)
@@ -57,8 +71,8 @@ func main() {
 		os.Exit(1)
 	}
 	if flags.isServer {
-		// runServer()
+		runServer(&flags)
 	} else {
-		// runClient()
+		runClient(&flags)
 	}
 }
