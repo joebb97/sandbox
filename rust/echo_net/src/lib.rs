b5736caf4b70361eea5415548b6a4fd6b638ba30
Joseph Buiteweg <joebb@umich.edu>
2021-01-15 12:18:02 -0500

    Command line args for echo server in rust

--- rust/echo_net/src/lib.rs
+++ rust/echo_net/src/lib.rs
@@ -1,1 +1,29 @@
-// Library file
+use clap::ArgMatches;
+use std::error::Error;
+
+pub fn run_server(app: ArgMatches) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+pub fn run_client(app: ArgMatches) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+// use getopts::Options;
+// use std::env;
+
+// fn main_optopt() -> Result<(), Box<dyn Error>>{
+//     let args: Vec<String> = env::args().collect();
+//     // let program = args[0].clone();
+
+//     let mut opts = Options::new();
+//     opts.optopt("", "proto", "transport layer protocol to use", "tcp");
+//     opts.optopt("", "port", "port to listen on or connect to", "5001");
+//     opts.optopt("a", "address", "address or hostname of server to connect to", "5001");
+//     opts.optflag("s", "server", "whether this is an echo server");
+//     opts.optflag("c", "client", "whether this is an echo client");
+//     let matches = opts.parse(&args[1..])?;
+//     println!("{:?}", matches);
+//     Ok(())
+// }
+//
--- rust/echo_net/src/main.rs
+++ rust/echo_net/src/main.rs
@@ -1,19 +1,53 @@
 extern crate getopts;
-use getopts::Options;
+use clap::{Arg, App};
+use echo_net::{run_server, run_client};
 use std::error::Error;
-use std::env;
 
-fn main() -> Result<(), Box<dyn Error>>{
-    let args: Vec<String> = env::args().collect();
-    // let program = args[0].clone();
-
-    let mut opts = Options::new();
-    opts.optopt("", "proto", "transport layer protocol to use", "tcp");
-    opts.optopt("", "port", "port to listen on or connect to", "5001");
-    opts.optopt("a", "address", "address or hostname of server to connect to", "5001");
-    opts.optflag("s", "server", "whether this is an echo server");
-    opts.optflag("c", "client", "whether this is an echo client");
-    let matches = opts.parse(&args[1..])?;
-    println!("{:?}", matches);
+fn main() -> Result<(), Box<dyn Error>> {
+    let matches = App::new("rust_echo_net")
+        .version("1.0")
+        .author("Joey Buiteweg <joebb@umich.edu>")
+        .about("Create an echo server or client to send messages with.")
+        .arg(Arg::new("proto")
+             .long("proto")
+             .value_name("tcp|udp")
+             .default_value("tcp")
+             .about("Set the transport layer protocol to either tcp or udp")
+             .takes_value(true))
+        .arg(Arg::new("port")
+             .long("port")
+             .value_name("PORT")
+             .default_value("5005")
+             .about("Set port to connect to/listen on")
+             .takes_value(true))
+        .arg(Arg::new("address")
+             .long("addr")
+             .short('a')
+             .value_name("ADDR")
+             .default_value("localhost")
+             .about("Set IP or hostname to connect to")
+             .takes_value(true))
+        .arg(Arg::new("server")
+             .short('s')
+             .long("server")
+             .takes_value(false)
+             .about("Specify whether this an echo server"))
+        .arg(Arg::new("client")
+             .short('c')
+             .long("client")
+             .takes_value(false)
+             .about("Specify whether this an echo client"))
+        .get_matches();
+    let server_set = matches.is_present("server");
+    let client_set = matches.is_present("client");
+    println!("{:?} {:?}", server_set, client_set);
+    if !(server_set || client_set) || (server_set && client_set) {
+        Err("Exactly one of -s|-c must be specified")?;
+    }
+    if server_set {
+        run_server(matches)?
+    } else if client_set {
+        run_client(matches)?
+    }
     Ok(())
 }
